a80e008f22ad7254dcdf9b4372847ba141fcbcf0
musthafa <musthafatn@scg.com>
2021-09-28 11:07:15 +0530

    demo commit

--- src/com/example/Calculator.java
+++ src/com/example/Calculator.java
@@ -10,7 +10,11 @@ public class Calculator {
 		final int a = s.nextInt();
 		final int b = s.nextInt();
 		final int c = add(a, b);
-		System.out.println(c);
+//		System.out.println(c);
+//		final var aaaaa = String.format("%s%n%s%nVAT Registration: %s%nBranch Code: %s", "aaaa", "bbbbb", "ccccc",
+//				"ddddddd");
+//		System.out.println(aaaaa);
+		System.out.println("hi");
 	}
 
 	public static int add(final int a, final int b) {
@@ -28,3 +32,27 @@ public class Calculator {
 	}
 
 }
+
+//cartResponse
+//.setPlatformShippingPromotion(getPlatformShippingPromotion(translations, cartWrapper.getCartData()));
+
+//private Map<String, Object> getPlatformShippingPromotion(final Map<String, String> translations,
+//final List<Map<String, Object>> cartData) {
+//for (final Map<String, Object> data : cartData) {
+//final List<Map<String, Object>> minValueShippingPromotions = new ArrayList<>();
+//final var shippingPromotions = ((List<Map<String, Object>>) data.get("shippingPromotions")).stream()
+//		.filter(promotion -> "platform".equals(promotion.get("createdBy"))).collect(Collectors.toList());
+//if (!shippingPromotions.isEmpty()) {
+//	var minValueShippingPromotion = shippingPromotions.get(0);
+//	var minimumOrderValue = (Double) minValueShippingPromotion.get("minimumOrderValue");
+//	for (final Map<String, Object> shippingPromotion : shippingPromotions) {
+//		if ((Double) shippingPromotion.get("minimumOrderValue") < minimumOrderValue) {
+//			minValueShippingPromotion = shippingPromotion;
+//			minimumOrderValue = (Double) minValueShippingPromotion.get("minimumOrderValue");
+//		}
+//	}
+//	minValueShippingPromotions.add(minValueShippingPromotion);
+//}
+//}
+//return null;
+//}
